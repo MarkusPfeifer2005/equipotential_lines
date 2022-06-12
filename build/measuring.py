668ecc1a0fcc8ea863c110c1921069acd92d5bf6
Markus Pfeifer <mkspfeifer@gmail.com>
2022-06-13 01:05:55 +0200

    fixed machine.map

--- build/measuring.py
+++ build/measuring.py
@@ -6,11 +6,14 @@ from RPi import GPIO as GPIO
 from hardware_accessories import StepperMotor, PushButton  # , Camera
 from data_accessories import RpiSession, JSON
 import ADS1x15
+
+
 # import torch
 
 
 class Master:
     """Communicates with raspberry pi pico via UART-protocol."""
+
     def __init__(self, port: str = "/dev/ttyS0", baudrate: int = 9600):
         self.ser = serial.Serial(port=port, baudrate=baudrate)
         print(self.ser)
@@ -41,11 +44,12 @@ class Actuator:
 
 class Machine:
     """Class to control the crane-like machine. The machine must be manually moved into its starting position."""
+
     def __init__(self, json: JSON):
         self.actuator = {
-            'x': Actuator(mot_pins=[7, 11, 13, 15], mot_reverse=False, gear_reduction=17*pi, p_button_pin=38),
+            'x': Actuator(mot_pins=[7, 11, 13, 15], mot_reverse=False, gear_reduction=17 * pi, p_button_pin=38),
             'y': Actuator(mot_pins=[12, 16, 18, 22], mot_reverse=True, gear_reduction=51, p_button_pin=40),
-            'z': Actuator(mot_pins=[19, 21, 23, 29], mot_reverse=False, gear_reduction=7.35*pi/2, p_button_pin=36)
+            'z': Actuator(mot_pins=[19, 21, 23, 29], mot_reverse=False, gear_reduction=7.35 * pi / 2, p_button_pin=36)
         }
         self.json = json
 
@@ -54,30 +58,42 @@ class Machine:
         for idx, actuator in enumerate(self.actuator.values()):
             distance = new_pos[idx] - self.pos[idx]  # calculate distance
             required_rotations = distance / actuator.gear_reduction  # calculate required rotations in deg
-            self.pos[idx] = actuator.mot.run_angle(required_rotations*360) / 360*actuator.gear_reduction + self.pos[idx]
+            self.pos[idx] = actuator.mot.run_angle(required_rotations * 360) / 360 * actuator.gear_reduction + self.pos[
+                idx]
 
     def map(self, measuring_function, measuring_kwargs: dict) -> None:
+        # plan path
+        positions = [[x, y, z]
+                     for z in range(0, self.json["area_to_map"][2], self.json["step_size"][2])
+                     for y in range(0, self.json["area_to_map"][1], self.json["step_size"][1])
+                     for x in range(0, self.json["area_to_map"][0], self.json["step_size"][0])]
+        if self.json["pos"] != [0, 0, 0]:
+            positions = positions[positions.index(self.json["pos"]):]
+
+        # initialize machine
         self.actuator['x'].mot.run_angle(angle=90, velocity=.5)  # get the motor away from push button
         self.actuator['x'].zero()
         self.actuator['y'].mot.run_angle(angle=90, velocity=.5)
         self.actuator['y'].zero()
         self.actuator['z'].mot.run_angle(angle=90, velocity=.5)
         self.actuator['z'].zero()
-        self.move_pos((0, 0, self.json["distance_to_liquid"]))
         self.pos = (0, 0, 0)
+        self.move_pos((0, 0, self.json["distance_to_liquid"]))
 
-        for z in tqdm(range(0, self.json["area_to_map"][2], self.json["step_size"][2])):
-            for y in range(0, self.json["area_to_map"][1], self.json["step_size"][1]):
-                for x in range(0, self.json["area_to_map"][0], self.json["step_size"][0]):
-                    self.move_pos((x, y, z))
-                    measuring_function(pos=(x, y, z), **measuring_kwargs)
-                    self.pos = (x, y, z)
+        # measure
+        for new_pos in tqdm(positions):
+            self.move_pos(new_pos)
+            measuring_function(pos=new_pos, **measuring_kwargs)
+            self.pos = new_pos
+
+            if self.pos[0] + self.json["step_size"][0] == self.json["area_to_map"][0]:
                 self.actuator['x'].zero()
                 self.pos = (0, self.pos[1], self.pos[2])  # the x pos must be set to 0
-            self.actuator['y'].zero()
-            self.pos = (self.pos[0], 0, self.pos[2])  # the y pos must be set to 0
+            if self.pos[1] + self.json["step_size"][1] == self.json["area_to_map"][1]:
+                self.actuator['y'].zero()
+                self.pos = (self.pos[0], 0, self.pos[2])  # the y pos must be set to 0
 
-        self.move_pos((0, 0, 0))
+        self.move_pos((0, 0, -self.json["distance_to_liquid"]//2))  # raise out of liquid to avoid corrosion
 
     @property
     def pos(self) -> list:
